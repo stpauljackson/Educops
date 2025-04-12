1132d05428ee0397aa59e2e606b56d15ec5755e2
stpauljackson <stpauljackson3@gmail.com>
2025-04-13 00:49:27 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/Educops' : '',
-  assetPrefix: isProd ? '/Educops/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
